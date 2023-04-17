2ccb82ec45a8115b0d87f12eba0ac16d224a57e8
Taehyun Kwon <taehkwon@c2r7s3.42seoul.kr>
2023-04-17 22:41:10 +0900

    frame created

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 22:21:07 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/04/17 22:39:14 by taehkwon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,1 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 21:56:37 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/04/17 22:40:35 by taehkwon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+char	*ft_strdup(const char *s1)
+{
+	char	*arr;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1);
+	arr = (char *)malloc((len + 1) * (sizeof(char)));
+	if (!(arr))
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		arr[i] = s1[i];
+		i++;
+	}
+	arr[len] = '\0';
+	return (arr);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*dst_p;
+	unsigned char	*src_p;
+
+	dst_p = (unsigned char *)dst;
+	src_p = (unsigned char *)src;
+	if (n == 0 || dst == src)
+		return (dst);
+	while (n--)
+	{
+		*dst_p = *src_p;
+		dst_p++;
+		src_p++;
+	}
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*result;
+
+	if (!s1 && !s2)
+		return (0);
+	else if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	result = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!result)
+		return (0);
+	ft_memcpy (result, s1, s1_len);
+	ft_memcpy (result + s1_len, s2, s2_len);
+	result[s1_len + s2_len] = '\0';
+	return (result);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s + i);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+	size_t	s_len;
+	size_t	result_len;
+
+	if (!s)
+		return (0);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	result_len = s_len - start;
+	if (result_len > len)
+		result_len = len;
+	result = (char *)malloc((result_len + 1) * sizeof(char));
+	if (!result)
+		return (0);
+	ft_memcpy(result, s + start, result_len);
+	result[result_len] = '\0';
+	return (result);
+}
